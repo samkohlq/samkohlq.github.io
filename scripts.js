2982660605d06ec97e2ef8f7eab4bfe606de571e
Samantha Koh <samanthakoh@Samanthas-MacBook-Pro.local>
2020-01-24 15:49:25 +0800

    add jquery function to populate project page with first item from json file

--- scripts.js
+++ scripts.js
@@ -2,3 +2,9 @@ function insertNavBar() {
   $("#navbar-side").load('navbar.html');
 }
 
+$(document).ready(function () {
+  $.getJSON("projects.json", function (data) {
+    var project = '<div>recreating ' + data[0].name + '</div>';
+    $("#project").html(project).addClass("text-secondary h2");
+  })
+})
