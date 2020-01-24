806f81a865f93c45c5fcb29aecc5fc6ffe71cacf
Philip Koh <philip.koh@techs-iMac-3.local>
2020-01-24 17:12:23 +0800

    remove individual project pages, add function to search for url param and populate page based on projectId

--- scripts.js
+++ scripts.js
@@ -2,14 +2,18 @@ function insertNavBar() {
   $("#navbar-side").load('navbar.html');
 }
 
+
+
 $(document).ready(function () {
   $.getJSON("projects.json", function (data) {
+    var urlParam = location.search;
+    var projectId = urlParam.charAt(urlParam.length-1);
     // adds div tag, taking project name from JSON file
-    var project = '<div class="text-seconary h2">recreating ' + data[0].name + '</div>';
+    var project = '<div class="text-seconary h2">recreating ' + data[projectId].name + '</div>';
     // adds para tag, taking project description form JSON file 
-    project += '<p class="text-secondary small">' + data[0].description + '</p>';
+    project += '<p class="text-secondary small">' + data[projectId].description + '</p>';
     // adds img tag, taking image source from JSON file
-    project += '<img class="img-fluid" src="' + data[0].img_src + '" alt="' + data[0].name + '">';
+    project += '<img class="img-fluid" src="' + data[projectId].img_src + '" alt="' + data[projectId].name + '">';
     $("#project").append(project);
   })
 })
