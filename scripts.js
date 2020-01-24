9bd93a2aceef7ed3531b313a10d1544062ef13cd
Philip Koh <philip.koh@techs-iMac-3.local>
2020-01-24 23:07:04 +0800

    fix function in scripts.js to populate page from JSON file only if user is on project.html

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
+// inserts navigation bar on every page
 function insertNavBar() {
   $("#navbar-side").load('navbar.html');
 }
 
-$(document).ready(function () {
-  $.getJSON("projects.json", function (data) {
-    var urlParam = location.search;
-    var projectId = urlParam.charAt(urlParam.length - 1);
-    // adds div tag, taking project name from JSON file
-    var project = '<div class="text-seconary h2">recreating ' + data[projectId].name + '</div>';
-    // adds para tag, taking project description form JSON file 
-    project += '<p class="text-secondary small">' + data[projectId].description + '</p>';
-    // adds img tag, taking image source from JSON file
-    project += '<img class="img-fluid" src="' + data[projectId].img_src + '" alt="' + data[projectId].name + '">';
-    $("#project").append(project);
+// only execute function to populate page with project details if we are on the project.html page
+if (window.location.pathname == '/project') {
+  $(document).ready(function () {
+    $.getJSON("projects.json", function (data) {
+      var urlParam = location.search;
+      var projectId = urlParam.charAt(urlParam.length - 1);
+      // adds div tag, taking project name from JSON file
+      var project = '<div class="text-seconary h2">recreating ' + data[projectId].name + '</div>';
+      // adds para tag, taking project description form JSON file 
+      project += '<p class="text-secondary small">' + data[projectId].description + '</p>';
+      // adds img tag, taking image source from JSON file
+      project += '<img class="img-fluid" src="' + data[projectId].img_src + '" alt="' + data[projectId].name + '">';
+      $("#project").append(project);
+      console.log(project);
+    })
   })
-})
+}
