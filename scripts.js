e18a7742fb2f00dc72b107bd20375e9d23844087
Samantha Koh <samanthakoh@Samanthas-MacBook-Pro.local>
2020-01-24 16:16:42 +0800

    add image key to JSON object

--- scripts.js
+++ scripts.js
@@ -4,7 +4,12 @@ function insertNavBar() {
 
 $(document).ready(function () {
   $.getJSON("projects.json", function (data) {
-    var project = '<div>recreating ' + data[0].name + '</div>';
-    $("#project").html(project).addClass("text-secondary h2");
+    // adds div tag, taking project name from JSON file
+    var project = '<div class="text-seconary h2">recreating ' + data[0].name + '</div>';
+    // adds para tag, taking project description form JSON file 
+    project += '<p class="text-secondary small">' + data[0].description + '</p>';
+    // adds img tag, taking image source from JSON file
+    project += '<img class="img-fluid" src="' + data[0].img_src + '" alt="' + data[0].name + '">';
+    $("#project").append(project);
   })
 })
