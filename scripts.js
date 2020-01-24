9d918759b33ba9b9bfe24f4ec8b42706b9a7f592
Samantha Koh <samanthakoh@Samanthas-MacBook-Pro.local>
2020-01-24 15:01:41 +0800

    remove jquery function to check for temperature in Pulau Ubin

--- scripts.js
+++ scripts.js
@@ -1,15 +1,4 @@
-// function insertNavBar() {
-//   $("#navbar-side").load('navbar.html');
-// }
-
 function insertNavBar() {
-  if ($("#navbar-side").load('../navbar.html') == true)
-    $("#navbar-side").load('../navbar.html');
+  $("#navbar-side").load('navbar.html');
 }
 
-function getWeatherData() {
-  $.get("https://api.data.gov.sg/v1/environment/air-temperature", function (data) {
-    const temp = data.items['0'].readings['6'].value;
-    $('#text-to-change').text("The temperature is..." + temp)
-  })
-}
