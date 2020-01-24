796d3f6e502a26e2ac6c343c652304834ad3214e
Philip Koh <philip.koh@techs-iMac-3.local>
2020-01-24 14:00:52 +0800

    add jquery function to insert navbar into all pages

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
+// function insertNavBar() {
+//   $("#navbar-side").load('navbar.html');
+// }
+
+function insertNavBar() {
+  if ($("#navbar-side").load('../navbar.html') == true)
+    $("#navbar-side").load('../navbar.html');
+}
+
 function getWeatherData() {
   $.get("https://api.data.gov.sg/v1/environment/air-temperature", function (data) {
-    console.log(data)
     const temp = data.items['0'].readings['6'].value;
     $('#text-to-change').text("The temperature is..." + temp)
   })
