ec05b3989410ae4cd624768f485917bf56590386
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-04-17 21:19:11 +0800

    remove newsfeed project

--- scripts.js
+++ scripts.js
@@ -4,16 +4,14 @@ function insertNavBar() {
 }
 
 // only execute function to populate page with project details if we are on the project.html page
-$(document).ready(function() {
+$(document).ready(function () {
   if ($("body").is(".project")) {
-    $.getJSON("projects.json", function(data) {
+    $.getJSON("projects.json", function (data) {
       var urlParam = location.search;
       var projectId = urlParam.charAt(urlParam.length - 1);
       // adds div tag, taking project name from JSON file
       var project =
-        '<div class="text-seconary h2">recreating ' +
-        data[projectId].name +
-        "</div>";
+        '<div class="text-seconary h2">' + data[projectId].name + "</div>";
       // adds para tag, taking project description form JSON file
       project +=
         '<p class="text-secondary small">' +
